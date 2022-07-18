04ade2b7545d4a8362b6096141e8c5ed8398806f
axitvadi-admin <axit.vadi@infyom.com>
2022-07-18 10:04:10 +0530

    fix: seeder index and error handler

--- app.js
+++ app.js
@@ -15,10 +15,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api', routes)
-
-app.use(errorHandler)
-
 seed.seedAdmin()
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
